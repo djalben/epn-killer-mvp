68256084c43d522b5f4ca5f0c19dd7af3eb65fe9
Александр Блонский <vardump@inbox.ru>
2026-02-10 23:31:57 +0300

    убрал Prometheus

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,6 @@ import (
 
 	logHandler "github.com/djalben/epn-killer-mvp/internal/lib/logger/handler"
 	libEnvironment "github.com/djalben/epn-killer-mvp/internal/lib/tools/environment"
-	logTools "github.com/djalben/epn-killer-mvp/internal/lib/tools/logger"
 	"github.com/djalben/epn-killer-mvp/internal/services"
 )
 
@@ -23,17 +22,10 @@ func main() {
 	defer cancel()
 
 	logLevel, logPlain := libEnvironment.GetLog()
-
-	handler, err := logTools.Prometheus(logHandler.Create(logPlain, logLevel))
-	if err != nil {
-		slog.ErrorContext(ctx, "logTools.Prometheus", slog.Any("error", err))
-
-		return
-	}
-
+	handler := logHandler.Create(logPlain, logLevel)
 	logger := slog.New(handler)
 
-	err = services.Run(ctx, logger)
+	err := services.Run(ctx, logger)
 	if err != nil {
 		logger.ErrorContext(ctx, "services.Run", slog.Any("error", err))
 
